3726686cf726d24e0fdd8f8c9999eabe5affe866
chemvic <chemvic79@gmail.com>
2023-06-02 19:39:12 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from './components/App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/test-task-cards">
+    <BrowserRouter basename="/test-task-tweetcards">
      <App /> 
     </BrowserRouter>
     
