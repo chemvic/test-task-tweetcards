df6da941cbe0ef17957b6e4fae4b63d0015662f1
chemvic <chemvic79@gmail.com>
2023-06-02 19:28:03 +0300

    залил старый src

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import css from './Home.module.css';
+
+const Home=() =>{
+    return (
+      <div className={css.container}>
+      <p className={css.title}>Tweet cards welcome page </p>      
+       
+      </div>
+    );
+  }
+  export default Home;
