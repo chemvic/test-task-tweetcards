6208779d07bf3c7cd80827131f47391d056a3241
chemvic <chemvic79@gmail.com>
2023-06-02 19:46:09 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import {useEffect, lazy} from 'react';
+import { lazy} from 'react';
 import SharedLayout from './SharedLayout';
 
 import css from './App.module.css';
